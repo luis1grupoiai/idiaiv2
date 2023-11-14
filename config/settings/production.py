99fffe50fe2f00642582dcdd700f7eb96f29f5d0
eloymendoza <146760444+eloymendoza@users.noreply.github.com>
2023-11-14 09:16:24 -0600

    Configurando development.py

---
+++ config/settings/production.py
@@ -0,0 +1,26 @@
+from .base import *
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+# TODO SECRET_KEY = 'django-insecure-=#m$s3h6y9%6ye(+e!y$*760)h@a!w)bf@&wo2nqjwn)9tkylp'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+# TODO DEBUG = True
+
+# TODO ALLOWED_HOSTS = []
+
+# Database
+# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'postgres',
+#         'USER': 'postgres',
+#         'PASSWORD': 'postgres',
+#         'HOST': 'db',
+#         'PORT': '5432',
+#     }
+# }  

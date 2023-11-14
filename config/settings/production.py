53439b3c8a20896baf2dfe776d0edf68a4c0d35e
eloymendoza <146760444+eloymendoza@users.noreply.github.com>
2023-11-14 09:29:11 -0600

    Configurando production.py

--- config/settings/production.py
+++ config/settings/production.py
@@ -1,26 +1,24 @@
 from .base import *
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
+import os
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# TODO SECRET_KEY = 'django-insecure-=#m$s3h6y9%6ye(+e!y$*760)h@a!w)bf@&wo2nqjwn)9tkylp'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# TODO DEBUG = True
+DEBUG = False
 
-# TODO ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'postgres',
-#         'USER': 'postgres',
-#         'PASSWORD': 'postgres',
-#         'HOST': 'db',
-#         'PORT': '5432',
-#     }
-# }  
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': os.environ.get('DB_PORT'),
+    }
+}  

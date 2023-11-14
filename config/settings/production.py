8a4a1f09c19ac339ad3deeb6cd5ffc753b79cb58
eloymendoza <146760444+eloymendoza@users.noreply.github.com>
2023-11-14 10:43:31 -0600

    Usando variables de entorno

--- config/settings/production.py
+++ config/settings/production.py
@@ -1,5 +1,9 @@
 from .base import *
+from dotenv import load_dotenv
 import os
+
+load_dotenv(Path.joinpath(BASE_DIR, '.env'))
+
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY')

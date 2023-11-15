102b08096f912fa034a75e5fd2e95334d248cc79
eloymendoza <146760444+eloymendoza@users.noreply.github.com>
2023-11-15 11:12:20 -0600

    Update

--- config/settings/production.py
+++ config/settings/production.py
@@ -29,6 +29,22 @@ DATABASES = {
     }
 }
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': os.environ.get('DB_ENGINE'),
+#         'NAME': os.environ.get('DB_NAME'),
+#         'USER': os.environ.get('DB_USER'),
+#         'PASSWORD': os.environ.get('DB_PASSWORD'),
+#         'HOST': os.environ.get('DB_HOST'),
+#         'PORT': os.environ.get('DB_PORT'),
+#         'OPTIONS':{
+#             'driver': 'ODBC Driver 17 for SQL Server',
+#             'unicode_results': True,
+           
+#         },
+#     },
+# }
+
 STATIC_ROOT=Path.joinpath(BASE_DIR, 'staticfiles')
 
 # STORAGES = {

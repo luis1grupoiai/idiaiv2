8ea7fca8495adb73a8bc27e7e69988a2dc6af753
eloymendoza <146760444+eloymendoza@users.noreply.github.com>
2023-11-16 11:35:49 -0600

    Configurando el .gitignore con Ruck

--- config/settings/development.py
+++ config/settings/development.py
@@ -1,4 +1,9 @@
-from .base import *
+import os
+from config.logging import *
+from config.settings.base import *
+from dotenv import load_dotenv
+
+load_dotenv(Path.joinpath(BASE_DIR, '.env'))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'miproyecto'
@@ -13,7 +18,15 @@ ALLOWED_HOSTS = []
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'ENGINE': 'mssql',
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': '',
+        'OPTIONS':{
+            'driver': 'ODBC Driver 17 for SQL Server',
+            'unicode_results': True,
+        },
+    },
 }

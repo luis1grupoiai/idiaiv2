53ac7dd29ab687eae4b28463ed3c5dd1c9ec6973
Ana Ruth Sanchez Iturbide <ana.sanchez@grupo-iai.com.mx>
2023-11-22 16:06:45 -0600

    Avance de reconocimiento facial

--- config/settings/base.py
+++ config/settings/base.py
@@ -54,8 +54,7 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, '/apps/core/templates'),
-                 os.path.join(BASE_DIR, '/apps/rfacial/templates')],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [

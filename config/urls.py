ee8f0701551f9ccc4fd577c19bb0d0854cb1ee00
eloymendoza <146760444+eloymendoza@users.noreply.github.com>
2023-11-28 08:52:30 -0600

    Actulizando el nombre de la app core a mycore

--- config/urls.py
+++ config/urls.py
@@ -6,6 +6,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('core/', include('apps.core.urls')),
+    path('mycore/', include('apps.mycore.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 

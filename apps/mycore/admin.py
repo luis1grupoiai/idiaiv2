35708533dc03731edb6fd114ca1aac2e625ce783
eloymendoza <146760444+eloymendoza@users.noreply.github.com>
2023-11-28 16:01:36 -0600

    Modificando el modelo User de Django

--- apps/mycore/admin.py
+++ apps/mycore/admin.py
@@ -1,3 +1,35 @@
-from django.contrib import admin
+# Relacion 1 a 1
+# from django.contrib import admin
+# from django.contrib.auth.admin import UserAdmin
+# from .models import User, Coordinacion
+# # Register your models here.
 
-# Register your models here.
+# admin.site.register(User)
+# admin.site.register(Coordinacion)
+
+# Opcion 2
+
+# from django.contrib import admin
+# from django.contrib.auth.admin import UserAdmin
+# from django.contrib.auth.models import User
+# from .models import UserCoordinacion, Coordinacion
+
+# class UserProfileInline(admin.StackedInline):
+#     model = UserCoordinacion
+#     can_delete = False
+#     verbose_name_plural = 'Coordinacion de usuario'
+
+# class CustomUserAdmin(UserAdmin):
+#     inlines = (UserProfileInline, )
+
+# # Desregistras el UserAdmin de Django
+# admin.site.unregister(User)
+
+# # Registra tu CustomUserAdmin
+# admin.site.register(User, CustomUserAdmin)
+# admin.site.register(UserCoordinacion)
+
+# # También registras el modelo Coordinacion si quieres gestionarlo desde el admin
+# admin.site.register(Coordinacion)
+
+# opcion 3
--- apps/mycore/models.py
+++ apps/mycore/models.py
@@ -1,3 +1,52 @@
-from django.db import models
+# Relacion 1 a 1
 
-# Create your models here.
+# from django.contrib.auth.models import AbstractUser
+# from django.db import models
+
+# class Coordinacion(models.Model):
+#     nombre = models.CharField(max_length=255)
+
+#     def __str__(self):
+#         return self.nombre
+
+# class User(AbstractUser):
+#     # Agrega tus campos personalizados aquí
+#     coordinacion = models.ForeignKey(Coordinacion, on_delete=models.SET_NULL, null=True, blank=True)
+
+#     def __str__(self):
+#         return 'Nombre: ' + self.first_name + ' ' + self.last_name + ' - Coordinación: ' + self.coordinacion.nombre
+
+# Opcion 2
+
+# from django.contrib.auth.models import User
+# from django.db import models
+
+# class UserCoordinacion(models.Model):
+#     user = models.OneToOneField(User, on_delete=models.CASCADE)
+#     coordinacion = models.ForeignKey('mycore.Coordinacion', on_delete=models.SET_NULL, null=True, blank=True)
+
+#     def __str__(self):
+#         return 'Nombre: ' + self.user.first_name + ' ' + self.user.last_name + ' - Coordinación: ' + self.coordinacion.nombre
+#     class Meta:
+#         # Personaliza cómo se mostrará el nombre del modelo en la interfaz de administración de Django
+#         verbose_name = 'Coordinación de usuarios'
+#         verbose_name_plural = 'Coordinación de usuarios'
+
+# class Coordinacion(models.Model):
+#     nombre = models.CharField(max_length=255)
+#     abreviatura = models.CharField(max_length=10)
+#     id_coordinador = models.ForeignKey(User, on_delete=models.CASCADE)
+#     created_at = models.DateTimeField(auto_now_add=True)
+#     updated_at = models.DateTimeField(auto_now=True)
+    
+
+#     def __str__(self):
+#         return self.nombre
+#     class Meta:
+#         # Personaliza cómo se mostrará el nombre del modelo en la interfaz de administración de Django
+#         verbose_name = 'Coordinación'
+#         verbose_name_plural = 'Coordinación'
+
+# opcion 3
+
+

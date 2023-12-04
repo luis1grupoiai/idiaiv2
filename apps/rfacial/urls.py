7a7ccd932d6fcf054f0a7055fc6b25aeeeb077ca
Ana Ruth Sanchez Iturbide <ana.sanchez@grupo-iai.com.mx>
2023-12-04 10:31:48 -0600

    Avance de API Auth!

--- apps/rfacial/urls.py
+++ apps/rfacial/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import CompanyView
+from .views import CAutenticacion
 
 urlpatterns = [
     # path('facial/', views.inicio, name='inicio'),
     # path('video/', views.video, name='video'),
     # path('webcam/', views.webcam, name='webcam'),
-    path('companies/', CompanyView.as_view(), name='companies_list')
+    path('auth/', CAutenticacion.as_view(), name='auth')
 ]

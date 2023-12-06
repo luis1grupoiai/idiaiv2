593d240ecafd89b0a4f7dc81746de17e3a75d8f8
Ana Ruth Sanchez Iturbide <ana.sanchez@grupo-iai.com.mx>
2023-12-06 13:41:47 -0600

    Documentación de Api Auth

--- apps/rfacial/views.py
+++ apps/rfacial/views.py
@@ -8,6 +8,17 @@ from django.http.response import JsonResponse
 from apps.areas.models import *
 from passlib.hash import django_pbkdf2_sha256 as handler
 
+from django.shortcuts import render
+from django.http import JsonResponse
+
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+
+from drf_yasg.utils import swagger_auto_schema
+from drf_yasg import openapi
+
+
 import json
 import os
 import base64
@@ -17,24 +28,49 @@ import base64
 # import os, urllib
 # import mediapipe as mp
 
+# def swagger_json(request):
+#         with open('swagger/apiAuth.json', 'r') as json_file:
+#             data = json.load(json_file)
+#         return JsonResponse(data)
 
-class CAutenticacion(View):
+class CAutenticacion(APIView):
+
 
     @method_decorator(csrf_exempt)
     def dispatch(self,request, *args, **kwargs):
         return super().dispatch(request, *args, **kwargs)
 
     def get(self,request):
-        usuarios = list(User.objects.values())
-
-        if len(usuarios)>0:
-            datos = {'message': 'Success','usuarios':usuarios}
-        else:
-            datos = {'message': 'Usuarios no encontrados.'}
+        # usuarios = list(User.objects.values())
+        datos = {'message': 'Conexion exitosa a API AUTH :)'}
+        # if len(usuarios)>0:
+        #     # datos = {'message': 'Success','usuarios':usuarios}
+        #     datos = {'message': 'Conexión exitosa :)'}
+        # else:
+        #     datos = {'message': 'Usuarios no encontrados.'}
 
         return JsonResponse(datos)
-
+       
+    @swagger_auto_schema(
+        request_body=openapi.Schema(
+            type=openapi.TYPE_OBJECT,
+            properties={
+                'X-CSRFToken': openapi.Schema(type=openapi.TYPE_STRING, description='Token de autenticación.'),
+                'user': openapi.Schema(type=openapi.TYPE_STRING, description='Nombre de usuario.'),
+                'password': openapi.Schema(type=openapi.TYPE_STRING, description='contraseña en base64.'),
+                'idSistema': openapi.Schema(type=openapi.TYPE_INTEGER, description='Id del sistema donde el usuario esta iniciando sesión.'),
+            },
+            required=['X-CSRFToken', 'user', 'password','idSistema']
+        ),
+        responses={200: 'Usuario loggeado exitosamente'},
+    )
     def post(self,request):
+        """
+        Realiza la validación de las credenciales de los usuarios.
+
+        Para realizar una creación exitosa, envía un objeto JSON con los siguientes campos:
+       
+        """
         #Metodo Post
         #Este metodo se encarga de validar las credenciales del ususario que se esta loggeando al sistema,
         # como resultado obtiene (mediante el userName, password, idSistema y Token) los permisos y grupos del usuario.
@@ -59,7 +95,7 @@ class CAutenticacion(View):
                     print("Las contraseñas son iguales")
                     datos = {'message': 'Success', 'datosUser': dUsuario}
                 else:
-                    datos = {'message': 'Ups, parece ser que los tokens no coinciden.'}
+                    datos = {'message': 'Ups, la contraseña es incorrecta.'}
         
             return JsonResponse(datos)
         except ValueError as error:
@@ -67,8 +103,11 @@ class CAutenticacion(View):
             return False
         
 
-    def put(self,request):
-        pass
+    # def put(self,request):
+    #     pass
 
-    def delete(self,request):
-        pass
+    # def delete(self,request):
+    #     pass
+
+    
+

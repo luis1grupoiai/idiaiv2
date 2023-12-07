3e2a7f65a82aa65c473b2bb69e729ec0d0f19af5
eloymendoza <146760444+eloymendoza@users.noreply.github.com>
2023-12-07 11:19:11 -0600

    Creacion de tabla SistemaPermiso

--- apps/sistemas/admin.py
+++ apps/sistemas/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Sistemas, SistemaPermiso
 
-# Register your models here.
+class SistemaPermisoInline(admin.TabularInline):
+    model = SistemaPermiso
+
+class SistemaAdmin(admin.ModelAdmin):
+    inlines = [SistemaPermisoInline]
+
+admin.site.register(Sistemas, SistemaAdmin)

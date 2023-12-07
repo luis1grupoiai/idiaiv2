677cfe465fed4036490bf91edabe164ee1b47399
manuel.zarate@grupo-iai.com.mx <manuel.zarate@grupo-iai.com.mx>
2023-12-07 14:36:57 -0600

    actualizacion

--- apps/ActiveDirectory/urls.py
+++ apps/ActiveDirectory/urls.py
@@ -4,7 +4,7 @@ from . import views
 #from django.conf import settings
 
 urlpatterns = [
-    path('', views.consultar_usuarios, name="usuarios" ),
+    path('', views.login_auth, name="usuarios" ),
     path('agregar-usuario/', views.agregar_usuario, name='agregar_usuario'),
     path('editar_usuario/', views.editar_usuario, name='editar_usuario'),
     path('activar_usuario/<str:nombre_usuario>/', views.activar_usuario, name='activar_usuario'),

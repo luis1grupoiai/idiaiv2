33ae77f86e21cbb2798694d41ec99b93fb52310c
manuel.zarate@grupo-iai.com.mx <manuel.zarate@grupo-iai.com.mx>
2023-12-06 18:03:06 -0600

    actualizacion de vista de AD

--- apps/ActiveDirectory/urls.py
+++ apps/ActiveDirectory/urls.py
@@ -5,5 +5,8 @@ from . import views
 
 urlpatterns = [
     path('', views.consultar_usuarios, name="usuarios" ),
-    path('agregar-usuario/', views.agregar_usuario, name='agregar_usuario')
+    path('agregar-usuario/', views.agregar_usuario, name='agregar_usuario'),
+    path('editar_usuario/', views.editar_usuario, name='editar_usuario'),
+    path('activar_usuario/<str:nombre_usuario>/', views.activar_usuario, name='activar_usuario'),
+    path('desactivar_usuario/<str:nombre_usuario>/', views.desactivar_usuario, name='desactivar_usuario'),
     ]

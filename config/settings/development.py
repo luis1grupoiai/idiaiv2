1a0da38ba0dbc1c6561e2e6e10e0141fafa16a1f
eloymendoza <146760444+eloymendoza@users.noreply.github.com>
2023-12-07 14:46:50 -0600

    Configurando MSSQL en Development

--- config/settings/development.py
+++ config/settings/development.py
@@ -16,33 +16,23 @@ ALLOWED_HOSTS = []
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'mssql',
-#         'NAME': os.environ.get('DB_NAME'),
-#         'USER': os.environ.get('DB_USER'),
-#         'PASSWORD': os.environ.get('DB_PASSWORD'),
-#         'HOST': os.environ.get('DB_HOST'),
-#         'PORT': '',
-#         'OPTIONS':{
-#             'driver': 'ODBC Driver 17 for SQL Server',
-#             'unicode_results': True,
-#         },
-#     },
-# }
-
-# Database
-# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'ENGINE': 'mssql',
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': '',
+        'OPTIONS':{
+            'driver': 'ODBC Driver 17 for SQL Server',
+            'unicode_results': True,
+        },
+    },
 }
+
+
 # Configuración de LDAP
-
-
 AD_SERVER = 'ldap://192.192.194.10'  # Cambia esto según tu servidor
 AD_PORT = 389  # El puerto por defecto es 389
 AD_USER = 'CN=desarrollo,CN=Users,DC=iai,DC=com,DC=mx'  # Cambia esto según tus credenciales

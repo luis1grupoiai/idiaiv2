505e517c4a09f4b77cd3eb467c77d4c7faf7fd48
eloymendoza <146760444+eloymendoza@users.noreply.github.com>
2023-12-11 10:55:23 -0600

    Documentando Admin y Models de Areas

--- apps/areas/models.py
+++ apps/areas/models.py
@@ -2,13 +2,19 @@ from django.contrib.auth.models import AbstractUser, User
 from django.db import models
 
 class UserCoordinacion(models.Model):
+    # Campo de clave única que establece una relación uno a uno con el modelo de usuario predeterminado de Django (User)
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    # Campo de clave externa que establece una relación muchos a uno con el modelo Coordinacion del módulo areas
+    # Si la Coordinacion asociada se elimina, el campo coordinacion se establecerá en NULL
     coordinacion = models.ForeignKey('areas.Coordinacion', on_delete=models.SET_NULL, null=True, blank=True)
 
+    # Método para obtener una representación de cadena del objeto UserCoordinacion
     def __str__(self):
+        # Devuelve el nombre del usuario y el nombre de la coordinación (o 'Sin coordinación' si no hay coordinación)
         return 'Nombre: ' + self.user.first_name + ' ' + self.user.last_name + ' - Coordinación: ' + (self.coordinacion.nombre if self.coordinacion else 'Sin coordinación')
+    
+    # Clase Meta que personaliza cómo se mostrará el nombre del modelo en la interfaz de administración de Django
     class Meta:
-        # Personaliza cómo se mostrará el nombre del modelo en la interfaz de administración de Django
         verbose_name = 'Coordinación de usuarios'
         verbose_name_plural = 'Coordinación de usuarios'
 

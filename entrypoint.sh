f088337569a4baecb8d91940bb9efe21e39d3afd
eloymendoza <146760444+eloymendoza@users.noreply.github.com>
2023-12-14 10:06:01 -0600

    Update

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+# . venv/Scripts/activate &
+
 echo 'Running collecstatic...'
 # python manage.py collectstatic --no-input --settings=config.settings.production
 python manage.py collectstatic --noinput --clear --verbosity 2 --settings=config.settings.production
@@ -10,3 +12,4 @@ python manage.py migrate --settings=config.settings.production
 
 echo 'Running server...'
 gunicorn -w 3 -t 120 --env DJANGO_SETTINGS_MODULE=config.settings.production config.wsgi:application --bind 0.0.0.0:333
+

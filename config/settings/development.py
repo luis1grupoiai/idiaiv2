a6806639defa96e28a1c8c6b7314fcebdde72d70
eloymendoza <146760444+eloymendoza@users.noreply.github.com>
2023-12-15 08:45:22 -0600

    Update Settings

--- config/settings/development.py
+++ config/settings/development.py
@@ -39,15 +39,13 @@ DATABASES = {
 #     }
 # }
 
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-# Configuración de LDAP
+STATIC_URL = '/static/'
 
-AD_SERVER = 'ldap://192.192.194.10'  # Cambia esto según tu servidor
-AD_PORT = 389  # El puerto por defecto es 389
-AD_USER = 'CN=desarrollo,CN=Users,DC=iai,DC=com,DC=mx'  # Cambia esto según tus credenciales
-#dsquery user -name desarrollo
-#dsget user "CN=desarrollo,CN=Users,DC=iai,DC=com,DC=mx"
-AD_PASSWORD = 'D3sarrollo'
-LOGIN_REDIRECT_URL = 'home' 
-LOGOUT_REDIRECT_URL = 'home'
+STATICFILES_DIRS = [
+    BASE_DIR / "components",
+]
+
 

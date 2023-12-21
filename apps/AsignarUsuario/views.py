7d0843eaabd52645150ed6084869395a2aa68ef9
Manuel.zarate <manue.zarate@grupo-iai.com.mx>
2023-12-21 08:41:11 -0600

    hola

--- apps/AsignarUsuario/views.py
+++ apps/AsignarUsuario/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout
-
+from .models import Vempleado
 @login_required
 def solicitud(request):
     # Aquí la lógica para mostrar la página de inicio
+    empleados = []
+    empleados=Vempleado.objects.all()
+    
+    print(empleados)
     context = {
+        'empleados' : empleados,
         'active_page': 'solicitud'
     }
     return render(request, 'personal.html',context)

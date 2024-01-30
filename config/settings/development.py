453ad625b189336ab858363a0080b93df3d03f4f
eloymendoza <146760444+eloymendoza@users.noreply.github.com>
2024-01-30 08:36:32 -0600

    Actualización n

--- config/settings/development.py
+++ config/settings/development.py
@@ -11,7 +11,7 @@ SECRET_KEY = 'miproyecto'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['grupo-iai.com.mx', 'www.grupo-iai.com.mx', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['grupo-iai.com.mx', 'www.grupo-iai.com.mx', 'iaipc130-pc.grupo-iai.com.mx', 'www.iaipc130-pc.grupo-iai.com.mx', '127.0.0.1', 'localhost']
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases

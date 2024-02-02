4008f9d4d0f25c6fa91b2788d4d55849d8d2e26d
Manuel.zarate <manue.zarate@grupo-iai.com.mx>
2024-02-02 08:57:55 -0600

    actualizacion de envio de correo

--- apps/ActiveDirectory/views.py
+++ apps/ActiveDirectory/views.py
@@ -653,3 +653,195 @@ def get_client_ip(request):
 
     # Si no se encuentra en las cabeceras, tomar la dirección del remitente de la solicitud
     return request.META.get('REMOTE_ADDR')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""
+librerias que hacen funcionar el proyecto ;) esto es un regalo de mi para el futuro XD
+absl-py                       2.0.0
+annotated-types               0.6.0
+asgiref                       3.7.2
+attrs                         23.1.0
+bcrypt                        4.1.1
+certifi                       2023.11.17
+cffi                          1.16.0
+charset-normalizer            3.3.2
+click                         8.1.7
+cmake                         3.27.7
+colorama                      0.4.6
+contourpy                     1.2.0
+cryptography                  41.0.7
+cycler                        0.12.1
+Django                        4.2.7
+django-components             0.29
+django-jazzmin                2.6.0
+django-mssql-backend          2.8.1
+django-querycount             0.8.3
+django-sslserver              0.22
+django-unfold                 0.18.0
+djangorestframework           3.14.0
+djangorestframework-simplejwt 5.3.1
+dlib                          19.24.2
+dotty-dict                    1.3.1
+drf-yasg                      1.21.7
+face-recognition              1.3.0
+face_recognition_models       0.3.0
+flatbuffers                   23.5.26
+fonttools                     4.46.0
+gitdb                         4.0.11
+GitPython                     3.1.40
+gunicorn                      21.2.0
+idna                          3.6
+importlib-resources           6.1.1
+inflection                    0.5.1
+Jinja2                        3.1.2
+kiwisolver                    1.4.5
+ldap3                         2.9.1
+markdown-it-py                3.0.0
+MarkupSafe                    2.1.3
+matplotlib                    3.8.2
+mdurl                         0.1.2
+mediapipe                     0.10.8
+mssql-django                  1.3
+numpy                         1.26.2
+opencv-contrib-python         4.8.1.78
+opencv-python                 4.8.1.78
+packaging                     23.2
+passlib                       1.7.4
+Pillow                        10.1.0
+pip                           23.3.2
+protobuf                      3.20.3
+psycopg2                      2.9.9
+pyasn1                        0.5.1
+pycparser                     2.21
+pycryptodome                  3.20.0
+pydantic                      2.5.3
+pydantic_core                 2.14.6
+Pygments                      2.17.2
+PyJWT                         2.8.0
+pyodbc                        5.0.1
+pyparsing                     3.1.1
+python-dateutil               2.8.2
+python-dotenv                 1.0.0
+python-gitlab                 4.3.0
+python-semantic-release       8.7.0
+pytz                          2023.3.post1
+PyYAML                        6.0.1
+requests                      2.31.0
+requests-toolbelt             1.0.0
+rich                          13.7.0
+setuptools                    65.5.0
+shellingham                   1.5.4
+six                           1.16.0
+smmap                         5.0.1
+sounddevice                   0.4.6
+sqlparse                      0.4.4
+tomlkit                       0.12.3
+typing_extensions             4.9.0
+tzdata                        2023.3
+uritemplate                   4.1.1
+urllib3                       2.1.0
+wfastcgi                      3.0.0
+whitenoise                    6.6.0
+"""
--- apps/AsignarUsuario/views.py
+++ apps/AsignarUsuario/views.py
@@ -10,7 +10,7 @@ from django.contrib import messages
 from django.core.mail import EmailMultiAlternatives
 from django.template.loader import get_template
 from django.utils.html import strip_tags
-
+from django.template.loader import render_to_string
 
 
 @login_required
@@ -56,65 +56,35 @@ def nameUser(request):
 
 
 
-
-
-
 def enviar_correo(request):
-    
-    print("se entro a la vista de envio de correo XD")
-
     if request.method == 'POST':
         dato1 = request.POST.get('nombre')
         dato2 = request.POST.get('puesto')
-        
-        mensaje = f'''
-        Asunto: Alta de Usuario
 
-        Hola,
+        # Preparar el contexto para la plantilla
+        context = {'nombre': dato1, 'puesto': dato2}
 
-        Aquí está el mensaje del correo con los datos:
+        # Renderizar el contenido HTML
+        html_content = render_to_string('CorreoSolicitudAlta.html', context)
+        text_content = strip_tags(html_content)  # Esto crea una versión en texto plano del HTML
 
-        Nombre: {dato1}
-        Puesto: {dato2}
-
-        Atentamente,
-        IDIAI
-        CONFIDENCIALIDAD. Este e-mail y cualquiera de sus archivos anexos son confidenciales y pueden constituir información privilegiada. Si usted no es el destinatario adecuado, por favor, notifíquelo inmediatamente al emisor y elimínelo de su computadora, no revele estos contenidos a ninguna otra persona, no los utilice para otra finalidad, ni almacene o copie esta información en medio alguno. 
-        CONFIDENTIALITY. This e-mail and any attachments thereof are confidential and may be privileged. If you are not a named recipient, please notify the sender immediately and delete it from your computer, do not disclose its contents to another person, use it for any purpose or store or copy the information in any medium
-
-        '''
-        
-        #codigo parar visualizar el correo que se va enviar 
-        datos = {
-        'nombre': dato1,
-        'puesto': dato2,
-         }
-        
-            
-        # Crear un mensaje HTML utilizando una plantilla (puedes personalizar la plantilla)
-        html_message = get_template('CorreoSolicitudAlta.html').render({'nombre': dato1, 'puesto': dato2})
-
-        # Crear el mensaje de texto plano
-        plain_message = strip_tags(html_message)
-        
+        # Crear el correo y añadir tanto el contenido en texto plano como el HTML
+        email = EmailMultiAlternatives(
+            'Asunto del Mensaje prueba',  # Asunto
+            text_content,  # Contenido en texto plano
+            'sistemas.iai@grupo-iai.com.mx',  # Email del remitente
+            ['manuel.zarate@grupo-iai.com.mx']  # Lista de destinatarios
+        )
+        email.attach_alternative(html_content, "text/html")
         try:
-            send_mail(
-                'Asunto del Mensaje prueba',
-                plain_message ,
-                'sistemas.iai@grupo-iai.com.mx',
-                ['manuel.zarate@grupo-iai.com.mx'],
-                fail_silently=False,
-            )
+            email.send()
             print("Correo enviado correctamente.")
         except Exception as e:
             print(f"Error al enviar correo: {e}")
-             
-    #return redirect(reverse('solicitud'))
-        
-    return render(request, 'CorreoSolicitudAlta.html', datos)
-        
-        
-        # Aquí puedes utilizar dato1 y dato2 como necesites
+
+    return render(request, 'CorreoSolicitudAlta.html', context)
+
+
 
 
     
